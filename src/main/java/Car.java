75c6a4604883d17040895a8ae6bbbb606cbd547f
anaypant <35899820+anaypant@users.noreply.github.com>
2021-11-01 11:50:57 -0700

    c

--- src/main/java/Car.java
+++ src/main/java/Car.java
@@ -2,11 +2,19 @@ public class Car extends Vehicle {
     // The Car class has no additional instance variables
 
     // Add a constructor that takes two parameters (maxSpeed and numWheels)
-
+    public Car(int maxSpeed, int numWheels){
+        super(maxSpeed, numWheels);
+    }
     // Add a constructor that takes one parameter (just speed),
     // the number of wheels is obvious for a car
+    public Car(int maxSpeed){
+        super(maxSpeed, 4);
+    }
 
     // Add a default constructor (no parameters)
+    public Car(){
+        super(100, 4);
+    }
 
     // No other code needed
 }
